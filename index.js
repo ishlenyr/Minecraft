d8407fbce43ee30a0f280888243a38f1725d366a
DDeenis <ddeenis46@gmail.com>
2022-12-08 10:48:14 +0200

    add room

---
+++ index.js
@@ -0,0 +1,35 @@
+const root = document.querySelector(":root");
+
+let varX = 300;
+let varY = 10;
+let varZ = 660;
+let rotateX = 0;
+
+document.addEventListener("keydown", (e) => {
+  console.log(e.key);
+  if (e.key === "ArrowLeft" && e.shiftKey) {
+    varY -= 2;
+    root.style.setProperty("--rotate-y", `${varY}deg`);
+  } else if (e.key === "ArrowRight" && e.shiftKey) {
+    varY += 2;
+    root.style.setProperty("--rotate-y", `${varY}deg`);
+  } else if (e.key === "ArrowRight") {
+    varZ -= 20;
+    root.style.setProperty("--translate-z", `${varZ}px`);
+  } else if (e.key === "ArrowLeft") {
+    varZ += 20;
+    root.style.setProperty("--translate-z", `${varZ}px`);
+  } else if (e.key === "ArrowDown" && e.shiftKey) {
+    rotateX -= 2;
+    root.style.setProperty("--rotate-x", `${rotateX}deg`);
+  } else if (e.key === "ArrowUp" && e.shiftKey) {
+    rotateX += 2;
+    root.style.setProperty("--rotate-x", `${rotateX}deg`);
+  } else if (e.key === "ArrowDown") {
+    varX -= 20;
+    root.style.setProperty("--translate-x", `${varX}px`);
+  } else if (e.key === "ArrowUp") {
+    varX += 20;
+    root.style.setProperty("--translate-x", `${varX}px`);
+  }
+});
